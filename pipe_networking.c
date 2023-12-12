2280d705553e76a1f021d0bc2c5055602e6b6f10
konstantinnovation <konstans@stuy.edu>
2023-12-12 13:38:21 -0500

    Create pipe_networking.c

---
+++ pipe_networking.c
@@ -0,0 +1,61 @@
+#include "pipe_networking.h"
+
+
+/*=========================
+  server_setup
+  args:
+
+  creates the WKP (upstream) and opens it, waiting for a  connection.
+  removes the WKP once a connection has been made
+
+  returns the file descriptor for the upstream pipe.
+  =========================*/
+int server_setup() {
+  int from_client = 0;
+  return from_client;
+}
+
+/*=========================
+  server_handshake
+  args: int * to_client
+
+  Performs the server side pipe 3 way handshake.
+  Sets *to_client to the file descriptor to the downstream pipe.
+
+  returns the file descriptor for the upstream pipe.
+  =========================*/
+int server_handshake(int *to_client) {
+  int from_client;
+  return from_client;
+}
+
+
+/*=========================
+  client_handshake
+  args: int * to_server
+
+  Performs the client side pipe 3 way handshake.
+  Sets *to_server to the file descriptor for the upstream pipe.
+
+  returns the file descriptor for the downstream pipe.
+  =========================*/
+int client_handshake(int *to_server) {
+  int from_server;
+  return from_server;
+}
+
+
+/*=========================
+  server_connect
+  args: int from_client
+
+  handles the subserver portion of the 3 way handshake
+
+  returns the file descriptor for the downstream pipe.
+  =========================*/
+int server_connect(int from_client) {
+  int to_client  = 0;
+  return to_client;
+}
+
+
